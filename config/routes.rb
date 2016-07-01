de9aae8ac12700886d6e231f1ff9a309600bb88f
Rails Udemy Course <railsudemycourse@gmail.com>
2016-07-01 18:45:53 +0000

    create new template, action and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -11,7 +13,7 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
+  resources :articles
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
   get 'contact', to: 'pages#contact'
