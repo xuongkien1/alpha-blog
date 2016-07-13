83143f8e5f992374d0946766e4b4f1864c53d32b
Rails Udemy Course <railsudemycourse@gmail.com>
2016-07-13 07:32:46 +0000

    Signup users form UI

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
   #   get 'products/:id' => 'catalog#view'
   root 'pages#homepage'
   get 'about', to: 'pages#about'
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
   resources :articles
