8fa2140728450fac243e99f69f2136d2941aa335
Rails Udemy Course <railsudemycourse@gmail.com>
2016-07-20 03:59:34 +0000

    Add sign in upon sign up

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160713050938) do
+ActiveRecord::Schema.define(version: 20160720011823) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
